fa8c058ee7d3060e86d9256dc3acdd37a911e95e
Tony <36971796+agtonybarletta@users.noreply.github.com>
2022-11-22 11:49:39 +0100

    Update install_basic.sh

--- install_basic.sh
+++ install_basic.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # install basics 
-sudo pacman -noconfirm -Sy git vi vim man base-devel tree htop curl wget neofetch
+sudo pacman --noconfirm -Sy git vi vim man base-devel tree htop curl wget neofetch
 
 # install yay
 git clone https://aur.archlinux.org/yay.git
